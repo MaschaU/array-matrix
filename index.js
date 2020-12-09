cf1c66792a2e2dc04e00c128826342bba9d066f2
Masa Utkovic <masa.utkovic@gmail.com>
2020-12-09 10:02:26 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var matrix = function generateArrays() {
+    for (var i=0; i<4; i++) {
+        for (var j=0; j<4; j++) {
+            print(0);
+        }
+    }
+}
+generateArrays();
+
+
+
+/*var array = function createMatrix(numOfRows, numOfColumns) {
+    for (var i=0; i<numOfRows; i++) {
+        var columns = [];
+        for (var j=0; j<numOfColumns; j++) 
+    }
+
+}*/
