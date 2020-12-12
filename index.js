419b271e4dce44b74f6b8702696b0bda7ddd58b4
Masa Utkovic <masautkovic@masas-air.localdomain>
2020-12-12 14:04:46 +0100

    generate matrix

--- index.js
+++ index.js
@@ -15,3 +15,24 @@
 //     }
 //   }
 // how's step 1 different than the preparation?
+const matrix = [];
+for (let r = 0; r < 4; r += 1) {
+  const row = [];
+  for (let c = 0; c < 4; c += 1) {
+    row.push(0);
+  }
+  matrix.push(row);
+}
+
+const createMatrix = (rowsCount, columnsCount) => {
+  const returned = [];
+  for (let r = 0; r < rowsCount; r += 1) {
+    const row = [];
+    for (let c = 0; c < columnsCount; c += 1) {
+      row.push(0);
+    }
+    returned.push(row);
+  }
+  return returned;
+};
+console.info(createMatrix(1, 3));
